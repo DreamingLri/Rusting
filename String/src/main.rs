bb53ba5cdbc9d7842e0bce8e4f4c3c60cd350bbe
DreamingLri <3507415455@qq.com>
2024-04-12 19:03:27 +0800

    add rev func

--- String/src/main.rs
+++ String/src/main.rs
@@ -2,7 +2,12 @@ fn main() {
     let data = "hello, world";
     let string = data.to_string();
 
-    let s1 = "Oops!".to_string();
+    let mut s1 = string.clone();
+    let mut s2 = String::new();
+    for i in s1.chars().rev() {
+        s2.push(i);
+    }
 
-    let s2 = String::from("hello");
+    println!("{}", string);
+    println!("{:?}", s2);
 }
